b9a96b3502309e67636e1344dc1f81d9f9bf51a6
William.Weng <William.Weng@iit.com.hk>
2025-03-12 15:59:58 +0800

    - 最低版本為 iOS 15

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,13 +6,13 @@ import PackageDescription
 let package = Package(
     name: "WWSimpleDeepSeekAI",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "WWSimpleDeepSeekAI", targets: ["WWSimpleDeepSeekAI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/William-Weng/WWNetworking.git", from: "1.6.2"),
+        .package(url: "https://github.com/William-Weng/WWNetworking.git", from: "1.7.3"),
     ],
     targets: [
         .target(name: "WWSimpleDeepSeekAI", dependencies: ["WWNetworking"], resources: [.copy("Privacy")]),
